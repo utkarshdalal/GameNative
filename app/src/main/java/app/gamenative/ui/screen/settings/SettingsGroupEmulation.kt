d842f93e4693e215d78fff683657042a0f776f36
Rodrigo Siviero <rodrigosiviero@gmail.com>
2025-12-03 18:19:44 -0300

    Adding import missing

--- app/src/main/java/app/gamenative/ui/screen/settings/SettingsGroupEmulation.kt
+++ app/src/main/java/app/gamenative/ui/screen/settings/SettingsGroupEmulation.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
