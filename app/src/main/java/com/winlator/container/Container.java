1ec8c74a862ed7f00b8044625f563fd8be2b1441
phobos665 <danielalexanderjoyce@gmail.com>
2025-12-13 14:26:04 +0000

    updated conditional

--- app/src/main/java/com/winlator/container/Container.java
+++ app/src/main/java/com/winlator/container/Container.java
@@ -863,10 +863,7 @@ public class Container {
                 if (dxwrapper.equals("original-wined3d")) {
                     data.put("dxwrapper", DEFAULT_DXWRAPPER);
                 }
-                else if (dxwrapper.startsWith("d8vk-") || dxwrapper.startsWith("dxvk-")) {
-                    data.put("dxwrapper", dxwrapper);
-                }
-                else if (dxwrapper.startsWith("d7vk-") || dxwrapper.startsWith("dxvk-")) {
+                else if (dxwrapper.startsWith("d8vk-") || dxwrapper.startsWith("d7vk-") || dxwrapper.startsWith("dxvk-")) {
                     data.put("dxwrapper", dxwrapper);
                 }
             }
