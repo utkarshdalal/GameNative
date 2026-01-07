e31ed75d571944666a2e98a1cfea8b7cec8addb6
phobos665 <danielalexanderjoyce@gmail.com>
2026-01-07 17:56:50 +0000

    Update xServerScreen to check for the wine version first.

--- app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
+++ app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
@@ -2304,10 +2304,10 @@ private fun extractx86_64InputDlls(context: Context, container: Container) {
     val imageFs = ImageFs.find(context)
     val wineVersion: String? = container.getWineVersion()
     Log.d("XServerDisplayActivity", "x86_64 Input DLL Extraction Verification: Container Wine version: " + wineVersion)
-    if ("proton-9.0-x86_64" == wineVersion) {
+    if ("proton-9.0-x86_64" == wineVersion || "proton-10-x86_64" == wineVersion) {
         val wineFolder: File = File(imageFs.getWinePath() + "/lib/wine/")
         Log.d("XServerDisplayActivity", "Extracting input dlls to " + wineFolder.getPath())
-    } else Log.d("XServerDisplayActivity", "Wine version is not proton-9.0-x86_64, skipping input dlls extraction")
+    } else Log.d("XServerDisplayActivity", "Wine version is not proton-9.0-x86_64 or proton-10-x86-64, skipping input dlls extraction")
 }
 
 private fun setupWineSystemFiles(
