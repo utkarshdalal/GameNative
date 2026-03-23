63845aa5f03d13fc7f78d7c2be4c3b6f982ec499
bart <bart.zaalberg@shift2.nl>
2026-03-23 20:45:58 +0100

    Nicer images for in GOG detail screens

--- app/src/main/java/app/gamenative/db/PluviaDatabase.kt
+++ app/src/main/java/app/gamenative/db/PluviaDatabase.kt
@@ -50,7 +50,7 @@ const val DATABASE_NAME = "pluvia.db"
         AmazonGame::class,
         DownloadingAppInfo::class
     ],
-    version = 13,
+    version = 14,
     // For db migration, visit https://developer.android.com/training/data-storage/room/migrating-db-versions for more information
     exportSchema = true, // It is better to handle db changes carefully, as GN is getting much more users.
     autoMigrations = [
@@ -60,6 +60,7 @@ const val DATABASE_NAME = "pluvia.db"
         AutoMigration(from = 10, to = 11),
         AutoMigration(from = 11, to = 12),
         AutoMigration(from = 12, to = 13), // Added amazon_games table
+        AutoMigration(from = 13, to = 14), // Added GOG background image column
     ]
 )
 @TypeConverters(
