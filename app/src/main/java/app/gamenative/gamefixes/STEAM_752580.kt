9f914089958894a36d4f788527b70d81fc7fb340
Misazam <misazam_93@hotmail.com>
2026-04-12 12:45:46 +0200

    Remove ini migration marker tracking

--- app/src/main/java/app/gamenative/gamefixes/STEAM_752580.kt
+++ app/src/main/java/app/gamenative/gamefixes/STEAM_752580.kt
@@ -2,8 +2,6 @@ package app.gamenative.gamefixes
 
 import app.gamenative.data.GameSource
 
-private const val IMPERIVM_SETTINGS_MIGRATION = "imperivm_audio_settings_v1"
-
 val STEAM_Fix_752580: KeyedGameFix = KeyedIniFileFix(
     gameSource = GameSource.STEAM,
     gameId = "752580",
@@ -13,5 +11,4 @@ val STEAM_Fix_752580: KeyedGameFix = KeyedIniFileFix(
         "SoundFX" to "1",
         "Speech" to "1",
     ),
-    migrationKey = IMPERIVM_SETTINGS_MIGRATION,
 )
