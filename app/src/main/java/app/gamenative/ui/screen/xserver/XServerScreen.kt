5eb8f238562089483b39e2f6dba3649b0c51257b
JT <297250+joshuatam@users.noreply.github.com>
2026-04-22 00:39:41 +0800

    Reapply "fix/feat: extract XAudio DLLs from DirectX redistributables (#1184)" (#1266)
    This reverts commit 93793fa33be80f49318b85726f67f6296f9b40db.

--- app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
+++ app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
@@ -1684,6 +1684,12 @@ fun XServerScreen(
                                 contentsManager,
                                 onExtractFileListener,
                             )
+
+                            if (container.wineVersion.lowercase().contains("proton-10")) {
+                                // Only proton 10 can apply this fix
+                                XServerScreenUtils.replaceXAudioDllsFromRedistributable(context, appId)
+                            }
+
                             extractArm64ecInputDLLs(context, container) // REQUIRED: Uses updated xinput1_3 main.c from x86_64 build, prevents crashes with 3+ players, avoids need for input shim dlls.
                             extractx86_64InputDlls(context, container)
                             extractGraphicsDriverFiles(
