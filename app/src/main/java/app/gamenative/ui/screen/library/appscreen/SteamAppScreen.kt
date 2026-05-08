e7486948c0b9ab5cc696fe05024e4d58c863b9aa
Raxcoms <mpaulo968@gmail.com>
2026-05-08 11:01:07 -0300

    Fallback to iconURL when clientIconURL is blank

--- app/src/main/java/app/gamenative/ui/screen/library/appscreen/SteamAppScreen.kt
+++ app/src/main/java/app/gamenative/ui/screen/library/appscreen/SteamAppScreen.kt
@@ -261,7 +261,11 @@ class SteamAppScreen : BaseAppScreen() {
 
         // Get icon URL
         val iconUrl = remember(appInfo.id) {
-            appInfo.clientIconUrl
+            if (appInfo.clientIconHash.isNotEmpty()) {
+                appInfo.clientIconUrl
+            } else{
+                appInfo.iconUrl
+            }
         }
 
         // Get install location
