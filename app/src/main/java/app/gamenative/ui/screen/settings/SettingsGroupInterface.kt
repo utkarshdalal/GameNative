e76fb3f9cc6f19b59c67125be980841e5334219f
Daniel Joyce <danielalexanderjoyce@gmail.com>
2026-06-09 10:22:47 +0100

    feat(): changed t o transparent for the settings group

--- app/src/main/java/app/gamenative/ui/screen/settings/SettingsGroupInterface.kt
+++ app/src/main/java/app/gamenative/ui/screen/settings/SettingsGroupInterface.kt
@@ -418,7 +418,7 @@ fun SettingsGroupInterface(
 
     // Custom Game Settings
     SettingsGroup(
-        modifier = Modifier.background(Color.Cyan),
+        modifier = Modifier.background(Color.Transparent),
         title = { Text(text = stringResource(R.string.settings_interface_custom_games)) },
     ) {
         var importCustomGameAsSteamGame by rememberSaveable { mutableStateOf(PrefManager.importCustomGameAsSteamGame) }
