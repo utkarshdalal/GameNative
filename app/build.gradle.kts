1369c7e91441de1c2aa6b6db3c61380729354e32
Utkarsh Dalal <dalal.utkarsh@gmail.com>
2026-07-25 18:41:47 -0700

    Bumped version code and link in readme (#1772)
    Co-authored-by: Utkarsh Dalal <utkarsh.dalal@toptal.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         buildConfigField("boolean", "XR_BUILD", "false")
         buildConfigField("boolean", "MODERN_XR", "false")
 
-        versionCode = 19
+        versionCode = 20
         versionName = "1.1.1"
 
         buildConfigField("boolean", "GOLD", "false")
