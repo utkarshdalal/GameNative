33c9391f5158c70ab09558aff86c049042a7a258
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-07-28 17:01:18 -0400

    Bundle 1/5-scale model-0 bionic-fg layer (43b88b3)

--- app/src/main/java/app/gamenative/utils/BionicFgManager.kt
+++ app/src/main/java/app/gamenative/utils/BionicFgManager.kt
@@ -56,7 +56,7 @@ object BionicFgManager {
     private const val ENV_CONFIG = "BIONIC_FG_CONFIG"
 
     // Current runtime version (bumped when the bundled .so changes)
-    private const val RUNTIME_VERSION = "9b17c8d-arm64-v8a"
+    private const val RUNTIME_VERSION = "43b88b3-arm64-v8a"
 
     // Asset paths
     private const val ASSET_DIR = "bionic_fg/android_arm64_v8a"
