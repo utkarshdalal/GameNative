4b637e301e69c7a62b9a4c2cf38faf605302900b
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-07-28 18:50:56 -0400

    Bundle 5x-flow-gain model-0 bionic-fg layer (40192e4)

--- app/src/main/java/app/gamenative/utils/BionicFgManager.kt
+++ app/src/main/java/app/gamenative/utils/BionicFgManager.kt
@@ -56,7 +56,7 @@ object BionicFgManager {
     private const val ENV_CONFIG = "BIONIC_FG_CONFIG"
 
     // Current runtime version (bumped when the bundled .so changes)
-    private const val RUNTIME_VERSION = "a491284-clean-arm64-v8a"
+    private const val RUNTIME_VERSION = "40192e4-clean-arm64-v8a"
 
     // Asset paths
     private const val ASSET_DIR = "bionic_fg/android_arm64_v8a"
