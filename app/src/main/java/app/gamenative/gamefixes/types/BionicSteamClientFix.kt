955175a206f47291882e5ee0629f9e2079e717c1
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-08-13 19:02:15 +0530

    Added bionic steam client gamefixes for Whisk and Whisk Demo

---
+++ app/src/main/java/app/gamenative/gamefixes/types/BionicSteamClientFix.kt
@@ -0,0 +1,36 @@
+package app.gamenative.gamefixes
+
+import android.content.Context
+import app.gamenative.data.GameSource
+import com.winlator.container.Container
+import timber.log.Timber
+
+class BionicSteamClientFix : GameFix {
+    override fun apply(
+        context: Context,
+        gameId: String,
+        installPath: String,
+        installPathWindows: String,
+        container: Container,
+    ): Boolean {
+        return try {
+            if (container.isLaunchBionicSteam) {
+                return true
+            }
+
+            container.setLaunchBionicSteam(true)
+            container.setLaunchRealSteam(false)
+            container.saveData()
+            Timber.tag("GameFixes").i("Enabled bionic Steam client for game $gameId")
+            true
+        } catch (e: Exception) {
+            Timber.tag("GameFixes").e(e, "Failed to enable bionic Steam client for game $gameId")
+            false
+        }
+    }
+}
+
+class KeyedBionicSteamClientFix(
+    override val gameSource: GameSource,
+    override val gameId: String,
+) : KeyedGameFix, GameFix by BionicSteamClientFix()
