d6673133bc14fba1cd02102d2f4d931e6c2a1be4
Joshua Tam <297250+joshuatam@users.noreply.github.com>
2026-08-17 21:52:31 +0800

    fix: match app language of the string resource in SteamService (#1817)

--- app/src/main/java/app/gamenative/service/SteamService.kt
+++ app/src/main/java/app/gamenative/service/SteamService.kt
@@ -57,6 +57,7 @@ import app.gamenative.utils.CaseInsensitiveFileSystem
 import app.gamenative.utils.ContainerUtils
 import app.gamenative.utils.FileUtils
 import app.gamenative.utils.LicenseSerializer
+import app.gamenative.utils.LocaleHelper
 import app.gamenative.utils.LsfgVkManager
 import app.gamenative.utils.MarkerUtils
 import app.gamenative.utils.Net
@@ -192,6 +193,13 @@ import java.nio.ByteOrder
 
 @AndroidEntryPoint
 class SteamService : Service(), IChallengeUrlChanged {
+
+    override fun attachBaseContext(newBase: Context) {
+        PrefManager.init(newBase)
+        val languageCode = PrefManager.appLanguage
+        val context = LocaleHelper.applyLanguage(newBase, languageCode)
+        super.attachBaseContext(context)
+    }
 
     // To view log messages in android logcat properly
     private val logger = object : LogListener {
