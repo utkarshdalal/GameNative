7a15d0911f4eee316da39e0b5a1aefecd45cf2e8
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-08-19 11:23:58 +0530

    bumped version for prerelease

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         buildConfigField("boolean", "MODERN_XR", "false")
 
         versionCode = 21
-        versionName = "1.1.1"
+        versionName = "1.2.0"
 
         buildConfigField("boolean", "GOLD", "false")
         fun secret(name: String) =
