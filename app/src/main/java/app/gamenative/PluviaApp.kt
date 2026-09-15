0bb5a4e17e14c6ecff2e91cd5d6b2495c6e4f2fa
david-kalbermatten <david.kalbermatten@hotmail.ch>
2026-09-15 22:23:21 +0200

    fix(app): Restore getAppContext in PluviaApp companion object

--- app/src/main/java/app/gamenative/PluviaApp.kt
+++ app/src/main/java/app/gamenative/PluviaApp.kt
@@ -213,6 +213,8 @@ class PluviaApp : SplitCompatApplication() {
         internal var onDestinationChangedListener: NavChangedListener? = null
 
         private lateinit var instance: PluviaApp
+        @JvmStatic
+        fun getAppContext(): android.content.Context? = if (::instance.isInitialized) instance.applicationContext else null
         private var cachedDefaultScreenSize: String? = null
 
         // TODO: find a way to make this saveable, this is terrible (leak that memory baby)
