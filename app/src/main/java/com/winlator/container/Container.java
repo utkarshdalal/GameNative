68518facd23e10676d393835a45e18fe21794667
david-kalbermatten <david.kalbermatten@hotmail.ch>
2026-09-15 22:07:36 +0200

    Revert "fix(container): Avoid hardcoded package path in DXVK cache and container data root"
    This reverts commit 2788724b1d635005c5574754e55790fa0318a83b.

--- app/src/main/java/com/winlator/container/Container.java
+++ app/src/main/java/com/winlator/container/Container.java
@@ -50,14 +50,12 @@ public class Container {
     public static final String DEFAULT_GRAPHICSDRIVERCONFIG = "vulkanVersion=1.3" + ",version=" + DefaultVersion.WRAPPER + ",blacklistedExtensions=" + ",maxDeviceMemory=0" + ",presentMode=mailbox" + ",syncFrame=0" + ",disablePresentWait=0" + ",resourceType=auto" + ",bcnEmulation=auto" + ",bcnEmulationType=compute" + ",bcnEmulationCache=0" + ",gpuName=Device";
     public static final String DEFAULT_WINCOMPONENTS = "direct3d=1,directsound=1,directinput8=0,directinput=0,directmusic=0,directshow=0,directplay=0,vcrun2010=1,wmdecoder=1,opengl=0";
     public static final String FALLBACK_WINCOMPONENTS = "direct3d=1,directsound=1,directinput8=0,directinput=0,directmusic=1,directshow=1,directplay=1,vcrun2010=1,wmdecoder=1,opengl=0";
-    public static final String DATA_ROOT = "/data/data/" + app.gamenative.BuildConfig.APPLICATION_ID;
-
     private static String resolveBaseFilesDir() {
         try {
             Context ctx = app.gamenative.PluviaApp.getAppContext();
             if (ctx != null) return ctx.getFilesDir().getAbsolutePath();
         } catch (Throwable ignored) {}
-        return DATA_ROOT + "/files";
+        return "/data/data/app.gamenative/files";
     }
 
     private static String resolveBaseStorageDir() {
@@ -70,7 +68,7 @@ public class Container {
                 }
             }
         } catch (Throwable ignored) {}
-        return DATA_ROOT + "/storage";
+        return "/data/data/app.gamenative/storage";
     }
 
     public static final String[] MEDIACONV_ENV_VARS = {
