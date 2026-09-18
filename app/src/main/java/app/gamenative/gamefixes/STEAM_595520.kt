a91c0b71dd7801b682f1f158a54a32470a19cc2b
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-09-18 20:48:07 +0530

    Added gamefix for saved games for FF XII

---
+++ app/src/main/java/app/gamenative/gamefixes/STEAM_595520.kt
@@ -0,0 +1,12 @@
+package app.gamenative.gamefixes
+
+import app.gamenative.data.GameSource
+
+/**
+ * FINAL FANTASY XII THE ZODIAC AGE (Steam)
+ */
+val STEAM_Fix_595520: KeyedGameFix = KeyedSaveFolderCollapseFix(
+    gameSource = GameSource.STEAM,
+    gameId = "595520",
+    documentsRelativePath = "My Games/FINAL FANTASY XII THE ZODIAC AGE",
+)
