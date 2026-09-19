3dbde1f1d004361cd8b62c7804dbeae0dc817c29
Joshua Tam <297250+joshuatam@users.noreply.github.com>
2026-09-19 16:44:05 +0800

    update pulseaudio to include module-pipe-source

--- app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
+++ app/src/main/java/app/gamenative/ui/screen/xserver/XServerScreen.kt
@@ -5467,7 +5467,7 @@ private suspend fun applyGeneralPatches(
 
 private fun refreshComponentsFiles(context: Context) {
     val extractionPairs = listOf(
-        "pulseaudio-gamenative-20260612.tzst" to File(context.filesDir, "pulseaudio")
+        "pulseaudio-gamenative-20260919.tzst" to File(context.filesDir, "pulseaudio")
     )
 
     AssetUtils.extractComponentsWithVersionCheck(
